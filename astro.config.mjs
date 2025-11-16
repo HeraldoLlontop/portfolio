73f846991ae05b1d0a5a266fa2ad5d39cdd038b4
HeraldoLlontop <hero_johan@hotmail.com>
2025-11-16 03:12:28 -0500

    chore: upgrade @astrojs/vercel to version 9.0.0 and update Astro dependency to version 5.15.8

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // If you are not going to use vercel analytics you can delete the analytics
 // If you want to remove vercel from your project you can do npm uninstall vercel
